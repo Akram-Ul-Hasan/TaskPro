a68bf7a14290958cc4ce304ca5bcc32587ff9cf1
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-05-20 20:12:10 +0600

    Initial Commit

---
+++ TaskPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskPro
+//
+//  Created by Akram Ul Hasan on 20/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskPro/TaskProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskProApp.swift
+//  TaskPro
+//
+//  Created by Akram Ul Hasan on 20/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
